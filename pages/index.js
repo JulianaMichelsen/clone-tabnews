8f6ad9b369a36594ebc00bdbef724eface88cfad
Juliana Michelsen <144944099+JulianaMichelsen@users.noreply.github.com>
2026-01-07 01:48:49 +0000

    adiciona scripts `lint:check`e `lint:fix`

--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,10 @@
 function Home() {
-    return <h1>Fer, bora pros 15 anos juntos? Te amo! Quero pelo menos mais 15 anos com você</h1>
+  return (
+    <h1>
+      Fer, bora pros 15 anos juntos? Te amo! Quero pelo menos mais 15 anos com
+      você
+    </h1>
+  );
 }
 
-export default Home;               
+export default Home;
